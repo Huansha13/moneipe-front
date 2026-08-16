68fd13580c06b11d83a65a28591fa30253cedd6d
Yefer Huansha <yhuansha@ebizlatin.com>
2026-08-15 19:31:48 -0500

    fix active route name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,10 @@ export const routes: Routes = [
     loadComponent: () => import('./features/dashboard/dashboard').then(m => m.Dashboard)
   },
   {
+    path: 'account',
+    loadComponent: () => import('./features/profile/acount/acount').then(m => m.Acount)
+  },
+  {
     path: 'login',
     loadComponent: () => import('./core/login/login').then(m => m.Login)
   }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal, inject, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
@@ -7,6 +7,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { ResponsiveService } from './shared/services/responsive.service';
 import {MatCardModule} from '@angular/material/card';
+import {filter, map} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,22 +18,24 @@ import {MatCardModule} from '@angular/material/card';
     MatIconModule,
     MatListModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   public responsive = inject(ResponsiveService);
+  private readonly router = inject(Router);
   sidenavOpened = signal(true);
+  activeRouteName = signal('');
 
   navItems = [
-    { icon: 'home', label: 'Home', active: true },
-    { icon: 'inbox', label: 'Inbox', badge: '3' },
-    { icon: 'search', label: 'Search' },
-    { icon: 'group', label: 'Team' },
-    { icon: 'notifications', label: 'Notifications' },
-    { icon: 'settings', label: 'Settings' },
+    { icon: 'dashboard', label: 'Dashboard', route: '/dashboard' },
+    { icon: 'account_balance', label: 'Cuentas y Finanzas', route: '/accounts' },
+    { icon: 'trending_up', label: 'Inversiones', route: '/investments' },
+    { icon: 'group', label: 'Mis Grupos', badge: '3', route: '/groups' },
   ];
 
   constructor() {
@@ -40,7 +43,23 @@ export class App {
     registry.setDefaultFontSetClass('material-symbols-outlined');
   }
 
+  ngOnInit(): void {
+    this.listenToRouteChanges();
+  }
+
   toggleSidenav() {
     this.sidenavOpened.update(v => !v);
+  }
+
+  listenToRouteChanges(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => event as NavigationEnd)
+    ).subscribe((event) => {
+      const rawName = event.urlAfterRedirects?.replaceAll('/', ' ').trim() || '';
+
+      const firstLetter = rawName.charAt(0).toUpperCase();
+      this.activeRouteName.set(`${firstLetter}${rawName.slice(1)}`);
+    });
   }
 }
